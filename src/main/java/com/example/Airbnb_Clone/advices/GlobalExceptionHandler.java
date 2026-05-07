6ce1fed733a049885bd4b209abe6e533c62ac006
Garv Sharma <Garv20sharma@gmail.com>
2026-05-07 10:49:45 +0530

    Almost done witht the Advices folder

--- src/main/java/com/example/Airbnb_Clone/advices/GlobalExceptionHandler.java
+++ src/main/java/com/example/Airbnb_Clone/advices/GlobalExceptionHandler.java
@@ -2,10 +2,16 @@ package com.example.Airbnb_Clone.advices;
 
 import com.example.Airbnb_Clone.exception.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authorization.AuthorizationDeniedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
@@ -29,5 +35,50 @@ public class GlobalExceptionHandler {
         }
     }
 
-    
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(AuthenticationException exception){
+        log.error("Unexpected error ocurred", exception);
+        ApiError apirError=ApiError.builder()
+                .httpstatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apirError);
+    }
+
+    @ExceptionHandler(JwtException.class)
+    public ResponseEntity<ApiResponse<?>> handleInternServerError(JwtException exception){
+        log.error("Unexpected error ocurred", exception);
+        ApiError apiError=ApiError.builder()
+                .httpstatus(HttpStatus.UNAUTHORIZED)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<ApiResponse<?>> handleAccessDeniedException(AuthorizationDeniedException exception){
+        log.error("Access denied 123", exception);
+        ApiError apiError=ApiError.builder()
+                .httpstatus(HttpStatus.FORBIDDEN)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotFoundException(MethodArgumentNotValidException exception) {
+        log.error("handleMethodArgumentNotFoundException: "+exception.toString());
+        List<String> errors = exception.getBindingResult().getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        ApiError apirError=ApiError.builder()
+                .httpstatus(HttpStatus.BAD_REQUEST)
+                .message("Input Validation failed")
+                .errors(errors)
+                .build();
+        return buildErrorResponseEntity(apirError);
+    }
+
+
 }
