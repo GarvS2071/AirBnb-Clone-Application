756cc84901f9f5bf9601cffcc73b52098f820e5a
Garv Sharma <Garv20sharma@gmail.com>
2026-05-08 12:40:19 +0530

    HoteBookingController done

---
+++ src/main/java/com/example/Airbnb_Clone/controllers/HotelBookingController.java
@@ -0,0 +1,46 @@
+package com.example.Airbnb_Clone.controllers;
+
+import com.example.Airbnb_Clone.dto.BookingDto;
+import com.example.Airbnb_Clone.dto.BookingRequest;
+import com.example.Airbnb_Clone.dto.GuestDto;
+import com.example.Airbnb_Clone.services.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/bookings")
+public class HotelBookingController {
+    private final BookingService bookingService;
+
+    @PostMapping
+    public ResponseEntity<BookingDto> initialiseBooking(@RequestBody BookingRequest bookingRequest) {
+        return ResponseEntity.ok(bookingService.initialiseBooking(bookingRequest));
+    }
+
+    @PostMapping("/{bookingId}/addGuests")
+    public ResponseEntity<BookingDto> addGuests(@PathVariable Long bookingId, @RequestBody List<GuestDto> guestDtoList) {
+        return ResponseEntity.ok(bookingService.addGuests(bookingId, guestDtoList));
+    }
+
+    @PostMapping("/{bookingId}/payments")
+    public ResponseEntity<Map<String, String>> initiatePayments(@PathVariable Long bookingId) {
+        String sessionUrl = bookingService.initiatePayments(bookingId);
+        return ResponseEntity.ok(Map.of("sessionUrl", sessionUrl));
+    }
+
+    @PostMapping("/{bookingId}/cancel")
+    public ResponseEntity<Map<String, String>> cancelBooking(@PathVariable Long bookingId) {
+        bookingService.cancelBooking(bookingId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{bookingId}/status")
+    public ResponseEntity<Map<String, String>> getBookingStatus(@PathVariable Long bookingId) {
+        return ResponseEntity.ok(bookingService.getBookingStatus(bookingId));
+    }
+}
