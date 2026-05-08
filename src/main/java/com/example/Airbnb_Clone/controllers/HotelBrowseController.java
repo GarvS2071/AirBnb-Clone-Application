b5918c224750156afe1f04e71ba4eda6bbabbd33
Garv Sharma <Garv20sharma@gmail.com>
2026-05-08 14:13:15 +0530

    Feat: Controller package almost finished

---
+++ src/main/java/com/example/Airbnb_Clone/controllers/HotelBrowseController.java
@@ -0,0 +1,30 @@
+package com.example.Airbnb_Clone.controllers;
+
+import com.example.Airbnb_Clone.dto.HotelInfoDto;
+import com.example.Airbnb_Clone.dto.HotelPriceDto;
+import com.example.Airbnb_Clone.dto.HotelSearchRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hotels")
+public class HotelBrowseController {
+
+    private final InventoryService inventoryService;
+
+    private final HotelService hotelService;
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<HotelPriceDto>> searchHotels(@RequestBody HotelSearchRequest hotelSearchRequest) {
+        Page<HotelPriceDto> page = inventoryService.searchHotels(hotelSearchRequest);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<HotelInfoDto> getHotelInfo(@PathVariable Long hotelId) {
+        return ResponseEntity.ok(hotelService.getHotelInfoById(hotelId));
+    }
+}
