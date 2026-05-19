9673a951957a296e1ad80e8cdad47e357e29f58e
Garv Sharma <Garv20sharma@gmail.com>
2026-05-19 11:00:14 +0530

    InventoryController Finished

---
+++ src/main/java/com/example/Airbnb_Clone/controllers/InventoryController.java
@@ -0,0 +1,26 @@
+package com.example.Airbnb_Clone.controllers;
+
+import com.example.Airbnb_Clone.dto.InventoryDto;
+import com.example.Airbnb_Clone.dto.UpdateInventoryRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+    private final InventoryService inventoryService;
+
+    @GetMapping("/rooms/{roomId}")
+    public ResponseEntity<List<InventoryDto>> getAllInventoryByRoom(@PathVariable Long roomId) {
+        return ResponseEntity.ok(inventoryService.getAllInventoryByRoom(roomId));
+    }
+
+    public ResponseEntity<Void> updateInventory(@PathVariable Long roomId, @RequestBody UpdateInventoryRequestDto updateInventoryRequestDto) {
+        inventoryService.updateInventory(roomId, updateInventoryRequestDto);
+        return ResponseEntity.noContent().build();
+    }
+}
